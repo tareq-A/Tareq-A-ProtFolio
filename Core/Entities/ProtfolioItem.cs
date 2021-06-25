0e9cbd97e0335b4dc187b0e85bebdc0548e96700
Tareq Abou Hjileh <Tareq.abou.hjileh@gmail.com>
2021-06-26 01:23:37 +0200

    Add project files.

---
+++ Core/Entities/ProtfolioItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class ProtfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
